5f5596983d70662684fee37508658e8b7c0140ed
Cariad Eccleston <cariad@cariad.earth>
2024-10-03 09:25:14 +0100

    Implement route to get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,13 +29,21 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   const book = books[req.params.isbn];
   if (!book) return res.status(404).json({ message: "Book not found" });
-  return res.send(books[req.params.isbn]);
+  return res.send(book);
  });
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const booksByAuthor = [];
+
+  for (isbn in books) {
+    const book = books[isbn];
+
+    if (book.author === req.params.author)
+      booksByAuthor.push(book);
+  }
+
+  return res.send(booksByAuthor);
 });
 
 // Get all books based on title
