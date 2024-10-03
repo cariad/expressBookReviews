a33453c9fd119f98976ee6d35c3bc816f4dfb458
Cariad Eccleston <cariad@cariad.earth>
2024-10-03 09:27:41 +0100

    Implement route to get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,8 +48,17 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const booksByTitle = [];
+
+  for (isbn in books) {
+    const book = books[isbn];
+
+    // @cariad
+    if (book.title === req.params.title)
+      booksByTitle.push(book);
+  }
+
+  return res.send(booksByTitle);
 });
 
 //  Get book review
